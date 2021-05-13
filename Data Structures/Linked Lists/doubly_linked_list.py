19cd39a70e3459d9407c63dbe3b5f614d659e1ab
Nipun Tewari <nipuntewari11@gmail.com>
2021-05-13 17:13:33 +0530

    finished

--- Data Structures/Linked Lists/doubly_linked_list.py
+++ Data Structures/Linked Lists/doubly_linked_list.py
@@ -2,9 +2,7 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
-        self.prev = None
-
-
+        self.prev = None 
 class DoublyLinkedList:
     def __init__(self):
         self.head = None
