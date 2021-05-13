6717ef8bb01e2d36439da980bc7c291a91b31424
Nipun Tewari <nipuntewari11@gmail.com>
2021-05-13 13:58:34 +0530

    add before tur

--- Data Structures/Linked Lists/doubly_linked_list.py
+++ Data Structures/Linked Lists/doubly_linked_list.py
@@ -37,7 +37,7 @@ class DoublyLinkedList:
         new_node.prev = None
         new_node.next = self.head
         self.head = new_node
-    
+
     def add_after_node(self, key, data):
         new_node = Node(data)
         if not self.head.next and self.head.data == key:
@@ -56,31 +56,23 @@ class DoublyLinkedList:
         else:
             new_node.next = current.next
             current.next = new_node
-            new_node.prev = current  
+            new_node.prev = current
 
     def add_before_node(self, key, data):
-        new_node = Node(data)
-        if not self.head.next and self.head.data == key:
-            self.head.prev = new_node
-            new_node.next = self.head
-            new_node.prev = None
-            self.head = new_node
-            return
-
         current = self.head
-        while current.next:
-            if current.data == key:
-                break
+        while current:
+            if current.prev is None and current.data == key:
+                self.prepend(data)
+                return
+            elif current.data == key:
+                new_node = Node(data)
+                prev = current.prev
+                prev.next = new_node
+                current.prev = new_node
+                new_node.next = current
+                new_node.prev = prev
+                return
             current = current.next
-        if not current.next and current.data != key:
-            print('Key does not exist')
-            return
-        
-        else:
-            new_node.prev = current.prev
-            current.prev.next = new_node
-            new_node.next = current
-
 
     def print_list(self):
         current = self.head
@@ -89,15 +81,22 @@ class DoublyLinkedList:
             current = current.next
 
 
+# dll = DoublyLinkedList()
+# dll.append(1)
+# dll.add_after_node(1, 11)
+# dll.append(2)
+# dll.add_after_node(2, 12)
+# dll.append(3)
+# dll.add_before_node(3, 13)
+# dll.append(4)
+# dll.append(5)
+# dll.prepend(0)
+# dll.add_after_node(5, 6)
+# dll.print_list()
+
 dll = DoublyLinkedList()
 dll.append(1)
-dll.add_after_node(1, 11)
 dll.append(2)
-dll.add_after_node(2, 12)
 dll.append(3)
-dll.add_before_node(4, 13)
 dll.append(4)
 dll.append(5)
-dll.prepend(0)
-dll.add_after_node(5, 6)
-dll.print_list()
