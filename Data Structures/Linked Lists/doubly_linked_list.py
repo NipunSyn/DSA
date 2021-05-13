cc21a65c92f51bb733ac228e37cc7a7977f19e53
Nipun Tewari <nipuntewari11@gmail.com>
2021-05-13 13:44:02 +0530

    add before self

--- Data Structures/Linked Lists/doubly_linked_list.py
+++ Data Structures/Linked Lists/doubly_linked_list.py
@@ -23,14 +23,64 @@ class DoublyLinkedList:
             prev = current
             current = current.next
         prev.next = new_node
+        new_node.prev = prev
         new_node.next = None
 
     def prepend(self, data):
         new_node = Node(data)
-        current = self.head
-        new_node.next = current
+        if not self.head:
+            new_node.prev = None
+            self.head = new_node
+            new_node.next = None
+            return
+        self.head.prev = new_node
         new_node.prev = None
+        new_node.next = self.head
         self.head = new_node
+    
+    def add_after_node(self, key, data):
+        new_node = Node(data)
+        if not self.head.next and self.head.data == key:
+            self.head.next = new_node
+            new_node.prev = self.head
+            new_node.next = None
+            return
+        current = self.head
+        while current.next:
+            if current.data == key:
+                break
+            current = current.next
+        if not current.next and current.data != key:
+            print('Key does not exist')
+            return
+        else:
+            new_node.next = current.next
+            current.next = new_node
+            new_node.prev = current  
+
+    def add_before_node(self, key, data):
+        new_node = Node(data)
+        if not self.head.next and self.head.data == key:
+            self.head.prev = new_node
+            new_node.next = self.head
+            new_node.prev = None
+            self.head = new_node
+            return
+
+        current = self.head
+        while current.next:
+            if current.data == key:
+                break
+            current = current.next
+        if not current.next and current.data != key:
+            print('Key does not exist')
+            return
+        
+        else:
+            new_node.prev = current.prev
+            current.prev.next = new_node
+            new_node.next = current
+
 
     def print_list(self):
         current = self.head
@@ -41,9 +91,13 @@ class DoublyLinkedList:
 
 dll = DoublyLinkedList()
 dll.append(1)
+dll.add_after_node(1, 11)
 dll.append(2)
+dll.add_after_node(2, 12)
 dll.append(3)
+dll.add_before_node(4, 13)
 dll.append(4)
 dll.append(5)
-dll.prepend(6)
+dll.prepend(0)
+dll.add_after_node(5, 6)
 dll.print_list()
